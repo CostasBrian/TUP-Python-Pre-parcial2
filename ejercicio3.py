4045dae652d485f305eb15a97a8073952111db01
CostasBrian <brian.costas.2014@gmail.com>
2022-06-16 15:19:52 -0300

    3.2 resuelto

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,14 +1,11 @@
-class Persona:
+class Persona():
     def __init__(self, nombre, edad, dni):
         self.nombre = nombre
         self.edad = edad
         self.dni = dni
         
-    def __str__(self):
-        return ('{} {} {}'.format(self.nombre, self.edad, self.dni))
-                
     def mostrar(self):
-        print(self.nombre, self.edad, self.dni)
+        return 'Nombre:{} - Edad:{} - DNI:{}'.format(self.nombre, self.edad, self.dni)
     
     def esMayorEdad(self):
         if int(self.edad) >= 18:
@@ -17,41 +14,41 @@ class Persona:
             return False
       
 ##------------------------------------------------------------------------
-class Cuenta(Persona):
+class Cuenta():
     def __init__(self, titular, cantidad):
         self.titular = titular
         self.cantidad = cantidad
     
-    def mostrarCuenta(self):
-        print(self.nombre, self.edad, self.dni, self.cantidad)
+    def mostrar(self):
+        return 'Cuenta\n Titular: {} - Saldo: ${}'.format(self.titular.mostrar(), self.cantidad)
     
     def ingresar(self, cantDinero):
         self.cantidad = self.cantidad + cantDinero
     
     def retirar(self, cantDinero):
         self.cantidad = self.cantidad - cantDinero
-        
-
 
 ##----------------------------------------------------------------------
 name = input("ingresarr nombre\n")
 age = input("ingresar edad\n")
 idn = input("ingresar dni\n")
+dinero = 11
 
-opcion = int(input("1_ingresar dinero\n2_retirar dinero\n"))
-if opcion == 1:
-    cantDinero = int(input("ingrese cantidad de dinero a ingresar\n"))
-    if cantDinero > 0:
-       
-        
-otra = Persona(name, age, idn)
-otra.mostrar()
+cliente = Persona(name, age, idn)   ##defino un objeto con la clase persona pasando datos personales
+clientefull = Cuenta(cliente,dinero)   ## defino un objeto de clase cuenta pasando el objeto anterior y cantidad de cuenta
+print(clientefull.mostrar())
 
-respuesta = otra.esMayorEdad()
-if respuesta:
-    print("es mayor de edad")
+##----------------------------------------------------------------------
+##defino opciones de carga y retiro
+if cliente.esMayorEdad():
+    opcion = int(input("1_ingresar dinero\n2_retirar dinero\n"))
+    cantDinero = int(input("ingrese cantidad de dinero\n"))
+    if opcion == 1:
+        if cantDinero > 0:
+            clientefull.ingresar(cantDinero)
+    else:
+        clientefull.retirar(cantDinero)
 else:
     print("no es mayor de edad")
     
-cliente= Cuenta(otra, dinero)
-print(cliente)
+print(clientefull.mostrar())
